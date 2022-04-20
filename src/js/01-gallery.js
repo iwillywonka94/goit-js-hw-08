8451359c46ee63fb632a69e0462f5283aaa68825
iwillywonka94 <iwillywonka94@gmail.com>
2022-04-20 16:17:14 +0300

    upd 1st hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,13 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+const itemsGallery = galleryItems.map(item => 
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+    </a>`
+).join("");
+gallery.insertAdjacentHTML("afterbegin", itemsGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250, showCounter: false});
