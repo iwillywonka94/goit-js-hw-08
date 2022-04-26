0d00fdeefbdec55ec7a26f4eb5ca47eaa7a82461
iwillywonka94 <iwillywonka94@gmail.com>
2022-04-26 15:22:21 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from "lodash.throttle";
+const data = localStorage.getItem("feedback-form-state")
+const array = {
+    email: "",
+    message: "",
+}
+const form = document.querySelector(".feedback-form")
+const arrayFill = function () {
+    array.email = form.elements.email.value;
+    array.message = form.elements.message.value;
+}
+form.addEventListener('input', throttle(() => {
+    arrayFill();
+    localStorage.setItem("feedback-form-state", JSON.stringify(array))
+}, 500))
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    form.reset();
+    localStorage.clear();
+    console.log(array)
+})
+if (data) {
+    form[0].value = JSON.parse(data).email;
+    form[1].value = JSON.parse(data).message;
+}
 
+arrayFill();
