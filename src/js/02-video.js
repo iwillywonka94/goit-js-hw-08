02d0ef997341fb827dc042f7ea6f4e4904161181
iwillywonka94 <iwillywonka94@gmail.com>
2022-04-26 08:58:03 +0300

    upd 2 HW

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player'; 
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000) );
+if (localStorage.getItem("videoplayer-current-time")) {
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time") )
+}
